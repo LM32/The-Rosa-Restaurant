c885ff2ae76ae4a2b23cf163feb5b458b05c4a84
LM32 <LMiranda32pm@gmail.com>
2021-11-15 17:12:42 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,91 @@
+//Select element function
+
+const selectElement = function (element) {
+    return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+menuToggler.addEventListener('click', function() {
+    body.classList.toggle('open');
+
+});
+
+
+
+
+//Added Event Listener to the window object to close the nav section
+//when the cursor pointer is anywhere within the body of the website
+window.addEventListener('click', function(e) {
+    let clickedElement = e.target;
+
+    if (clickedElement.matches('.is-open')) {
+        bodyElement.classList.remove('is-open');
+        heroElement.classList.remove('is-open');
+
+    }
+});
+
+
+//time @ 34:00 for menu option
+
+
+//Scroll Reveal Start
+
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+
+});
+
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+
+});
+
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+
+});
+
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+
+});
+
+//Scroll reveal End
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+    } else {
+    mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
